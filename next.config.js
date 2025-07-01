48b18c96a5c153dc72080cde3d07a6d818b0b7ec
naveenclearfeed <naveen@clearfeed.ai>
2025-07-01 20:45:19 +0530

    new changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
-    unoptimized: true,
+    unoptimized: false,
   },
 };
 
